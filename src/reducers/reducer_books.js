2e23429e1a0dc90bdf2cf032ab2a563d96d0cf79
Terje Sakariassen <terje@sakariassen.guru>
2018-09-06 14:25:33 +0200

    added book list component and redux

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { title: "Mistborn" },
+        { title: "Harry Potter" },
+        { title: "The Dark Tower" },
+        { title: "Lord of the Rings" }
+    ]
+}

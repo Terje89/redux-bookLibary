dfebc4dab3d95a36e852fffe327c6689c35437d7
Terje Sakariassen <terje@sakariassen.guru>
2018-09-07 10:52:12 +0200

    added active books reducer

--- src/reducers/reducer_active_book.js
+++ src/reducers/reducer_active_book.js
@@ -4,7 +4,7 @@ export default function(state = null, action) {
 
     switch(action.type) {
         case 'BOOK_SELECTED':
-            return bookList.selected;
+            return action.payload;
     }
 
     return state
